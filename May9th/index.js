60c9d4d6a9690109f5c7b057ff8f322e9fa40d87
xani-dev <xani.gn@gmail.com>
2023-05-09 11:08:12 -0400

    First Commit

---
+++ May9th/index.js
@@ -0,0 +1,17 @@
+let num = 3;
+let person1 = { firstName: 'Alice' };
+let person2 = { firstName: 'Bob' };
+
+
+const changeStuff = (a, b, c) => {
+	a = a * 10; //Primitive type, passed by copy
+	console.log(a); //This is inside the function, it will be changed
+	b.firstName = 'ALICE'; //Reference type, updates are propagted
+	c = { firstName: 'Charle' }; //Reference type: New creations are not propagated
+};
+
+changeStuff(num, person1, person2);
+
+console.log(num); //num is now outside of the function's scope, the change won't persist
+console.log(person1.firstName);
+console.log(person2.firstName);
