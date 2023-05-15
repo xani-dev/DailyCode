cecd81fb3d970cb6760883cb291896715dba0584
xani-dev <xani.gn@gmail.com>
2023-05-15 17:44:46 -0400

    Adding username welcome message

--- May15th/pre-react/script.js
+++ May15th/pre-react/script.js
@@ -1,10 +1,12 @@
 const userNameInput = document.getElementById('username');
 const passwordInput = document.getElementById('password');
 const submitBtn = document.getElementById('submitBtn');
+const welcome = document.getElementById('welcomeText')
 
 submitBtn.addEventListener('click', () => {
 	const username = userNameInput.value;
 	const password = passwordInput.value;
 
+    welcome.innerText=(`Welcome  ${username}!`)
 	console.log(`Username: ${username}, Password: ${password}`);
 });
