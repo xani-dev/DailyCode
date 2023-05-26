28dd330a5f92d9936834069b906ccc659ee9eb1a
xani-dev <xani.gn@gmail.com>
2023-05-26 12:57:09 -0400

    useEffect Exercises

--- May22nd/test-app2/src/Pages/AboutPage.js
+++ May22nd/test-app2/src/Pages/AboutPage.js
@@ -1,29 +1,77 @@
 import React, { useEffect, useState } from 'react';
-
+import axios from 'axios';
 
 export const AboutPage = () => {
-	let [text, setText] = useState('');
+	let [employees, setEmployees] = useState('');
 
-	useEffect(()=>{
-		console.log('I have been loaded into the DOM')
-	}, [])  //=> Empty dependency array
-
-	useEffect(()=>{
-		if(text){
-			console.log('Text has changed')
-		}
-	}, [text]) //Monitors changes in text
-
-	const handleClick = ()=> {
-		setText('changed')
-	}
+	useEffect(() => {
+		const getData = async () => {
+			const users = (
+				await axios.get('https://jsonplaceholder.typicode.com/users')
+			).data;
+			setEmployees(users);
+			console.log(users);
+		};
+		getData();
+	}, []); //=> Empty dependency array
 
 	return (
 		<div>
-			<h1>AboutPage</h1>
+			<h1>Employees</h1>
 			<br />
-
-			<button onClick={()=>{handleClick()}}>Click</button>
+			<div></div>
+			<table>
+				<tbody>
+					<tr>
+						<th>Name</th>
+						<th>Mail</th>
+					</tr>
+					<tr>
+						<td>{employees[0]?.name}</td>
+						<td>{employees[0]?.email}</td>
+					</tr>
+					<tr>
+						<td>{employees[1]?.name}</td>
+						<td>{employees[1]?.email}</td>
+					</tr>
+					<tr>
+						<td>{employees[2]?.name}</td>
+						<td>{employees[2]?.email}</td>
+					</tr>
+					<tr>
+						<td>{employees[3]?.name}</td>
+						<td>{employees[3]?.email}</td>
+					</tr>
+					<tr>
+						<td>{employees[4]?.name}</td>
+						<td>{employees[4]?.email}</td>
+					</tr>
+					<tr>
+						<td>{employees[5]?.name}</td>
+						<td>{employees[5]?.email}</td>
+					</tr>
+					<tr>
+						<td>{employees[6]?.name}</td>
+						<td>{employees[6]?.email}</td>
+					</tr>
+					<tr>
+						<td>{employees[7]?.name}</td>
+						<td>{employees[7]?.email}</td>
+					</tr>
+					<tr>
+						<td>{employees[8]?.name}</td>
+						<td>{employees[8]?.email}</td>
+					</tr>
+					<tr>
+						<td>{employees[9]?.name}</td>
+						<td>{employees[9]?.email}</td>
+					</tr>
+					<tr>
+						<td>{employees[10]?.name}</td>
+						<td>{employees[10]?.email}</td>
+					</tr>
+				</tbody>
+			</table>
 		</div>
 	);
 };
