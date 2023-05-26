63e5ff4b999bb97964586a0a2fb9876ea02900e6
xani-dev <xani.gn@gmail.com>
2023-05-26 15:46:00 -0400

    Adding Map to Object

--- May22nd/test-app2/src/Pages/AboutPage.js
+++ May22nd/test-app2/src/Pages/AboutPage.js
@@ -2,76 +2,42 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 export const AboutPage = () => {
-	let [employees, setEmployees] = useState('');
+	let [employees, setEmployees] = useState([]);
 
 	useEffect(() => {
-		const getData = async () => {
-			const users = (
-				await axios.get('https://jsonplaceholder.typicode.com/users')
-			).data;
+		const getEmployeeData = async () => {
+			const users = 
+				(await axios.get(`https://jsonplaceholder.typicode.com/users`)).data;
+			console.log(typeof users);
 			setEmployees(users);
-			console.log(users);
 		};
-		getData();
+		getEmployeeData();
+		console.log(employees);
 	}, []); //=> Empty dependency array
 
 	return (
-		<div>
+		<>
 			<h1>Employees</h1>
 			<br />
-			<div></div>
 			<table>
 				<tbody>
 					<tr>
 						<th>Name</th>
 						<th>Mail</th>
 					</tr>
-					<tr>
-						<td>{employees[0]?.name}</td>
-						<td>{employees[0]?.email}</td>
-					</tr>
-					<tr>
-						<td>{employees[1]?.name}</td>
-						<td>{employees[1]?.email}</td>
-					</tr>
-					<tr>
-						<td>{employees[2]?.name}</td>
-						<td>{employees[2]?.email}</td>
-					</tr>
-					<tr>
-						<td>{employees[3]?.name}</td>
-						<td>{employees[3]?.email}</td>
-					</tr>
-					<tr>
-						<td>{employees[4]?.name}</td>
-						<td>{employees[4]?.email}</td>
-					</tr>
-					<tr>
-						<td>{employees[5]?.name}</td>
-						<td>{employees[5]?.email}</td>
-					</tr>
-					<tr>
-						<td>{employees[6]?.name}</td>
-						<td>{employees[6]?.email}</td>
-					</tr>
-					<tr>
-						<td>{employees[7]?.name}</td>
-						<td>{employees[7]?.email}</td>
-					</tr>
-					<tr>
-						<td>{employees[8]?.name}</td>
-						<td>{employees[8]?.email}</td>
-					</tr>
-					<tr>
-						<td>{employees[9]?.name}</td>
-						<td>{employees[9]?.email}</td>
-					</tr>
-					<tr>
-						<td>{employees[10]?.name}</td>
-						<td>{employees[10]?.email}</td>
-					</tr>
+					{employees.map((employee) => {
+						return (
+							<tr>
+								<td>{employee?.name}</td>
+								<br></br>
+								<td>{employee?.email}</td>
+							</tr>
+						);
+					})}
+					<tr></tr>
 				</tbody>
 			</table>
-		</div>
+			
+		</>
 	);
 };
