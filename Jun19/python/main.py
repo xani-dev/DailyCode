16eb47bb0f0dbac159469c2ed91aba0d874f38dd
xani-dev <xani.gn@gmail.com>
2023-06-20 10:58:32 -0400

    Python Practice

---
+++ Jun19/python/main.py
@@ -0,0 +1,48 @@
+# # This program prints Hello, world!
+# print('Hello, world!')
+
+# message = 'Hello Python World'
+# message2 = 'Hello Xani'
+
+# print(message + ' & ' + message2)
+
+# def main():
+#     # int
+#     num1 = 10 
+#     # float
+#     num2 = 50.6
+#     # string
+#     num3 = '20' 
+
+#     sum1 = num1 + num2
+#     print((sum1))
+
+# main()
+
+
+# Slicing Example
+# def main():
+#     message = "This is a test."
+#     print(message[0:4])
+
+
+# if __name__ == '__main__':
+#     main()
+    
+# String to upper case
+def main():
+    message = 'This is a test.'
+    new_message = message.upper()
+    print(new_message)
+
+if __name__ == '__main__':
+    main()
+
+
+# String interpolation
+num1 = 10
+message = f'You won {num1} times'
+print (message)
+
+result =20/(8 + 2)
+print(result)
