92b9ce2ef844ea0b3205bdd9736387f3515e0044
xani-dev <xani.gn@gmail.com>
2023-06-20 17:14:32 -0400

    Jun20 Proactices

---
+++ Jun20/controlFlow.py
@@ -0,0 +1,25 @@
+
+# If Statement
+# def main():
+#     name = input('Enter Your Name: ')
+#     if name == 'Xani':
+#         print('Hello Xani!')
+#     elif name == 'Edgar':
+#         print('Hello Edgar')
+#     else:
+#         print('Hello Stranger!')
+
+# if __name__ == '__main__':
+#     main()
+
+
+# Loops
+def main1():
+    for x in range(3):
+        print(x+9)
+if __name__ == '__main__':
+    main1()
+
+fruits = []
+fruits.append('apple')
+print(fruits)
