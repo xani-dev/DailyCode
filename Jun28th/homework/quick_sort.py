6fe92db8a55c71ca22975c5a4ee024b0e006c887
xani-dev <xani.gn@gmail.com>
2023-06-28 20:47:11 -0400

    Daily code Jun28th

---
+++ Jun28th/homework/quick_sort.py
@@ -0,0 +1,33 @@
+#06.28.23 Homework Quick Sort Algorithm
+
+def partition(arr, low, high):
+    # index of smaller element
+	i = (low-1)	
+ 	# pivot	 
+	pivot = arr[high]	 
+
+	for j in range(low, high):
+
+		# If current element is smaller than or equal to pivot
+		if arr[j] <= pivot:
+
+			# increment index of smaller element
+			i = i+1
+			arr[i], arr[j] = arr[j], arr[i]
+
+	arr[i+1], arr[high] = arr[high], arr[i+1]
+	return (i+1)
+
+def quickSort(arr, low, high):
+	if len(arr) == 1:
+		return arr
+	if low < high:
+
+		pi = partition(arr, low, high)
+		quickSort(arr, low, pi-1)
+		quickSort(arr, pi+1, high)
+
+arr = [80, -1,5,-10,3,100,8,22]
+n = len(arr)
+quickSort(arr, 0, n-1)
+print(arr)
