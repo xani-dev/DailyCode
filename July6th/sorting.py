3bca2ef26952816da4a16d86fde0c6a15d198b78
xani-dev <xani.gn@gmail.com>
2023-07-06 17:46:27 -0400

    July6th Try it Out

---
+++ July6th/sorting.py
@@ -0,0 +1,22 @@
+def my_max(nums):        
+    nums.sort()
+    return('largest number: ',nums[-1])
+
+
+def my_max_even(nums):
+    even = []
+    
+    for num in nums:
+        if num%2==0:
+            even.append(num)
+    return('largest even number: ',even[-1])
+
+def main():
+    numbers = [5, 2, 8, 1, 3, 15]
+    max_number = my_max(numbers)
+    max_number_even=my_max_even(numbers)
+    print(max_number) 
+    print(max_number_even) 
+
+    
+main()
