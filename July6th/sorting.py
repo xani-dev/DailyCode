0e9a09739e6dc3560627820dccc3b500594fca04
xani-dev <xani.gn@gmail.com>
2023-07-07 10:23:32 -0400

    Adding Bubble Sort Example

--- July6th/sorting.py
+++ July6th/sorting.py
@@ -11,10 +11,25 @@ def my_max_even(nums):
             even.append(num)
     return('largest even number: ',even[-1])
 
+
+#Bubble Sort
+def bubble_sort(numbers):
+    n = len(numbers)
+    for i in range(n):
+        # Last i element is already in place.
+        for j in range(n-i-1):  
+            # Swap adjacent elements if in wrong order.
+            if numbers[j] > numbers[j+1]:  
+                numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
+    return numbers
+
+
 def main():
     numbers = [5, 2, 8, 1, 3, 15]
     max_number = my_max(numbers)
     max_number_even=my_max_even(numbers)
+    sorted_numbers = bubble_sort(numbers)
+    print(sorted_numbers)
     print(max_number) 
     print(max_number_even) 
 
